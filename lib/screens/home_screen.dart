a24c7c6f7d6656b8f642a084d14abf1f3e7fe4d6
siwapun.kh <work.siwapun.kh@gmail.com>
2024-05-01 20:06:02 +0700

    add jitsi_meet_v1, file widget meeting

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/history_meeting_screen.dart';
+import 'package:zoom_clone/utils/colors.dart';
+
+import 'meeting_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,12 +12,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentindex = 0;
+
+  List<Widget> pages = [
+    const MeetingScreen(),
+    const HistoryMeetingScreen(),
+    const Text('data'),
+    const Text('setting'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('data'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          title: const Text('Meet & Chat'),
+          centerTitle: true,
+        ),
+        body: pages[_currentindex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentindex,
+          onTap: (value) {
+            print(value);
+            setState(() {
+              _currentindex = value;
+            });
+          },
+          backgroundColor: footerColor,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          selectedFontSize: 14.0,
+          items: [
+            Bnv(const Icon(Icons.comment_bank), 'Meet & Char'),
+            Bnv(const Icon(Icons.lock_clock), 'Meeting'),
+            Bnv(const Icon(Icons.person_outline), 'Contects'),
+            Bnv(const Icon(Icons.settings_outlined), 'Setting'),
+          ],
+        ));
+  }
+
+  BottomNavigationBarItem Bnv(Icon icon, String lable) {
+    return BottomNavigationBarItem(icon: icon, label: lable);
   }
 }
